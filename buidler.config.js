587e445983fb1a285db7d4a522b23b09bcd522f2
PWho <patrick@patrickwho.me>
2020-10-21 16:26:56 -0400

    Downgrade solc version for imported contracts

--- buidler.config.js
+++ buidler.config.js
@@ -2,7 +2,7 @@ usePlugin("@nomiclabs/buidler-truffle5")
 
 module.exports = {
     solc: {
-        version: "0.7.1",
+        version: "0.6.2",
         optimizer: {
             enabled: true,
             runs: 200
